cf1ccfeb1c3e3fbec57ddb7f9fcf8a5f95b76e31
jj1165922611 <1165922611@qq.com>
2021-02-26 21:49:56 +0800

    os_linux

--- os_linux/first.py
+++ os_linux/first.py
@@ -8,7 +8,7 @@
 import os
 
 
-def first(path):
+def first(path,zippath):
     '''
     1.查找/tomcat/log目录下的log文件，
     如果文件最后修改时间是在一小时之前，把此文件打包压缩，备份到/home/back/log目录下
@@ -18,4 +18,9 @@ def first(path):
     file_list=os.listdir(path)
 
     for file in file_list:
-        os.stat("")
+        file_path=os.path.join(path, file)
+
+        time_list=os.stat(file_path)
+
+        if time_list[0]<1:
+            os.system(f"tar -zcvf {time_list} {zippath}")
